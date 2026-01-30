4b26f21e5f3900aab78931fe4114d50a5228790e
ola <olimpaww@gmail.com>
2026-01-30 18:03:02 +0100

    delete files

--- GazeLocalization/tests_run.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import tests_gaze_localizator
-from tests_gaze_localizator import SummaryTextTestRunner
-
-if __name__ == "__main__":
-    suite = unittest.defaultTestLoader.loadTestsFromModule(tests_gaze_localizator)
-    runner = SummaryTextTestRunner(verbosity=2, descriptions=True, buffer=True)
-    runner.run(suite)
--- README.md
+++
@@ -1,2 +0,0 @@
-# EyeTracker-MemoryGameAnalyses
-Project focusing on eye tracking as a tool for analyzing visual behavior of players in a Memory Game
